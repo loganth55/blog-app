d4f8b9e99fdd13fa09c3af773279fac891b0a72c
loganth55 <loganthpranav2005@gmail.com>
2026-06-22 01:26:25 +0530

    Remove duplicate frontend and backend folders

--- backend/model/subsriberSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const subscriberSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("subscriberSchema", subscriberSchema)
